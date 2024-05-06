05fc6bbcfbf66cd4ac5eb215ea10087c1ee993da
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-05-06 15:06:43 +0200

    React Fragments

--- 03-pizza-menu/starter/src/index.js
+++ 03-pizza-menu/starter/src/index.js
@@ -61,12 +61,19 @@ function Menu() {
 	return (
 		<main className="menu">
 			<h2>Our menu</h2>
+
 			{numPizzas > 0 ? (
-				<ul className="pizzas">
-					{pizzaData.map((pizza) => (
-						<Pizza pizzaObj={pizza} key={pizza.name} />
-					))}
-				</ul>
+				<>
+					<p>
+						Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all
+						delicious.
+					</p>
+					<ul className="pizzas">
+						{pizzaData.map((pizza) => (
+							<Pizza pizzaObj={pizza} key={pizza.name} />
+						))}
+					</ul>
+				</>
 			) : (
 				<p>No pizzas :(</p>
 			)}
