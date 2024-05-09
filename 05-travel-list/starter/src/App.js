b5abe23881fbb8a4cda41d9eba708a597464bce2
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-05-09 12:14:17 +0200

    Rendering the item list

--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+	{ id: 1, description: "Passports", quantity: 2, packed: false },
+	{ id: 2, description: "Socks", quantity: 12, packed: false },
+	{ id: 3, description: "Underwear", quantity: 12, packed: true },
+];
+
 export default function App() {
 	return (
 		<div className="app">
@@ -19,7 +25,25 @@ function Form() {
 	);
 }
 function PackingList() {
-	return <div className="list">LIST</div>;
+	return (
+		<div className="list">
+			<ul>
+				{initialItems.map((item) => (
+					<Item item={item} />
+				))}
+			</ul>
+		</div>
+	);
+}
+function Item({ item }) {
+	return (
+		<li>
+			<span style={item.packed ? { textDecoration: "line-through" } : {}}>
+				{item.quantity} {item.description}
+			</span>
+			<button>❌</button>
+		</li>
+	);
 }
 function Stats() {
 	return (
