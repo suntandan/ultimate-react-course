9cab86ce5b31e0bc41dc961e9da3ade0c03ecd39
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-06-01 17:32:01 +0200

    Added event listener for escape keydown

--- 07-usepopcorn/starter/src/App.js
+++ 07-usepopcorn/starter/src/App.js
@@ -1,50 +1,6 @@
 import { useEffect, useState } from "react";
 import StarRating from "./StarRating";
 
-const tempMovieData = [
-	{
-		imdbID: "tt1375666",
-		Title: "Inception",
-		Year: "2010",
-		Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-	},
-	{
-		imdbID: "tt0133093",
-		Title: "The Matrix",
-		Year: "1999",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-	},
-	{
-		imdbID: "tt6751668",
-		Title: "Parasite",
-		Year: "2019",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-	},
-];
-
-const tempWatchedData = [
-	{
-		imdbID: "tt1375666",
-		Title: "Inception",
-		Year: "2010",
-		Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-		runtime: 148,
-		imdbRating: 8.8,
-		userRating: 10,
-	},
-	{
-		imdbID: "tt0088763",
-		Title: "Back to the Future",
-		Year: "1985",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-		runtime: 116,
-		imdbRating: 8.5,
-		userRating: 9,
-	},
-];
 const average = (arr) => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 const KEY = "8724f54";
@@ -55,7 +11,6 @@ export default function App() {
 	const [watched, setWatched] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState("");
-	const tempQuery = "Interstellar";
 	const [selectedId, setSelectedId] = useState(null);
 
 	function handleSelectMovie(id) {
@@ -76,21 +31,25 @@ export default function App() {
 
 	useEffect(
 		function () {
+			const controller = new AbortController();
+
 			async function fetchMovies() {
 				try {
 					setIsLoading(true);
 					setError("");
-					const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+					const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`, { signal: controller.signal });
 
 					if (!res.ok) throw new Error("Failed to fetch movies");
 
 					const data = await res.json();
 					if (data.Response === "False") throw new Error(" Movie not found");
 					setMovies(data.Search);
-					console.log(data);
+					setError("");
 				} catch (err) {
 					console.error(err.message);
-					setError(err.message);
+					if (err.name !== "AbortError") {
+						setError(err.message);
+					}
 				} finally {
 					setIsLoading(false);
 				}
@@ -100,7 +59,12 @@ export default function App() {
 				setError("");
 				return;
 			}
+			handleCloseMovie();
 			fetchMovies();
+
+			return function () {
+				controller.abort();
+			};
 		},
 		[query]
 	);
@@ -286,7 +250,6 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 		imdbRating,
 		Plot: plot,
 		Released: released,
-		Actors: actors,
 		Director: director,
 		Genre: genre,
 		Runtime: runtime,
@@ -305,6 +268,23 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 		onAddWatched(newWatchedMovie);
 		onCloseMovie();
 	}
+
+	useEffect(
+		function () {
+			function callback(e) {
+				if (e.code === "Escape") {
+					onCloseMovie();
+				}
+			}
+			document.addEventListener("keydown", callback);
+
+			return function () {
+				document.removeEventListener("keydown", callback);
+			};
+		},
+		[onCloseMovie]
+	);
+
 	useEffect(
 		function () {
 			async function getMovieDetails() {
@@ -317,6 +297,18 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 			getMovieDetails();
 		},
 		[selectedId]
+	);
+
+	useEffect(
+		function () {
+			if (!title) return;
+			document.title = `${title || "Movie Details"} - usePopcorn`;
+
+			return function () {
+				document.title = "usePopcorn";
+			};
+		},
+		[title]
 	);
 	return (
 		<div className="details">
