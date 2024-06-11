ca2f4977ceba4c924d4d309177d1b2c52fafe8fe
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-06-11 16:50:34 +0200

    CSS Modules and setting up project, started nav

--- 11-worldwise/starter/worldwise/src/App.jsx
+++ 11-worldwise/starter/worldwise/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Product from './pages/Product'
 import Pricing from './pages/Pricing'
+import Product from './pages/Product'
+import Login from './pages/Login'
+import AppLayout from './pages/AppLayout'
 import Homepage from './pages/Homepage'
 import PageNotFound from './pages/PageNotFound' 
 
@@ -10,8 +12,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="product" element={<Product />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes> 
       </BrowserRouter>
