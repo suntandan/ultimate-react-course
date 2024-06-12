056538cc21e8f0e9dc7511aebf0aded6f54b6919
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-06-12 15:48:38 +0200

    Feat: Added dynamic URL with useParams

--- 11-worldwise/starter/worldwise/src/App.jsx
+++ 11-worldwise/starter/worldwise/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Pricing from './pages/Pricing'
 import Product from './pages/Product'
@@ -5,9 +6,32 @@ import Login from './pages/Login'
 import AppLayout from './pages/AppLayout'
 import Homepage from './pages/Homepage'
 import PageNotFound from './pages/PageNotFound' 
+import CityList from './components/CityList'
+import CountryList from './components/CountryList'
+import City from './components/City'
+
+const BASE_URL = 'http://localhost:3001'
 
 function App() {
-  return (
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert('Error loading data...')
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+  return ( 
     <div>
       <BrowserRouter>
         <Routes>
@@ -16,9 +40,10 @@ function App() {
           <Route path="product" element={<Product />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}> 
-            <Route index element={<p>List of cities</p>} /> 
-            <Route path="cities" element={<p>List of cities</p>} /> 
-            <Route path="countries" element={<p>Countries</p>} /> 
+            <Route index element={<CityList cities={cities} isLoading={isLoading} />} /> 
+            <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} /> 
+            <Route path="cities/:id" element={<City />}/>
+            <Route path="countries" element={<CountryList cities={cities}  />} /> 
             <Route path="form" element={<p>Form</p>} /> 
           </Route>
           <Route path="*" element={<PageNotFound />} />
