d70deba01ca97c02902e82db9be813773927b0ec
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-07-13 00:20:48 +0200

    233. Setting Map Position with Geolocation

--- 11-worldwise/starter/worldwise/src/components/Map.jsx
+++ 11-worldwise/starter/worldwise/src/components/Map.jsx
@@ -1,19 +1,43 @@
 import styles from "./Map.module.css";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
+import { useGeolocation } from "./hooks/useGeoLocation";
+import Button from "./Button";
 
 function Map() {
 	const navigate = useNavigate();
 	const { cities } = useCities();
 	const [mapPosition, setMapPosition] = useState([40, 0]);
 	const [searchParams, setSearchParams] = useSearchParams();
-	const lat = searchParams.get("lat");
-	const lng = searchParams.get("lng");
+	const { isLoading: isLoadingPosition, position: geolocationPosition, getPosition } = useGeolocation();
 
+	const mapLat = searchParams.get("lat");
+	const mapLng = searchParams.get("lng");
+
+	useEffect(
+		function () {
+			if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+		},
+		[mapLat, mapLng]
+	);
+
+	useEffect(
+		function () {
+			if (geolocationPosition) {
+				setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+			}
+		},
+		[geolocationPosition]
+	);
 	return (
 		<div className={styles.mapContainer} onClick={() => navigate("form")}>
+			{!geolocationPosition && (
+				<Button type="position" onClick={getPosition}>
+					{isLoadingPosition ? "Loading..." : "Use current position"}
+				</Button>
+			)}
 			<MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
 				<TileLayer
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -27,9 +51,27 @@ function Map() {
 						</Popup>
 					</Marker>
 				))}
+				<ChangeCenter position={mapPosition} />
+				<DetectClick />
 			</MapContainer>
 		</div>
 	);
+}
+
+function ChangeCenter({ position }) {
+	const map = useMap();
+	map.setView(position);
+	return null;
+}
+
+function DetectClick() {
+	const navigate = useNavigate();
+
+	useMapEvents({
+		click: (e) => {
+			navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+		},
+	});
 }
 
 export default Map;
