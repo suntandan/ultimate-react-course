2b2468187831fa6469781922b4837b02049a945d
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-07-13 16:52:50 +0200

    236. Deleting cities

--- 11-worldwise/starter/worldwise/src/contexts/CitiesContext.jsx
+++ 11-worldwise/starter/worldwise/src/contexts/CitiesContext.jsx
@@ -48,13 +48,26 @@ function CitiesProvider({ children }) {
 			const data = await res.json();
 			setCities((cities) => [...cities, data]);
 		} catch {
-			alert("Error loading data...");
+			alert("Error creating city.");
+		} finally {
+			setIsLoading(false);
+		}
+	}
+	async function deleteCity(id) {
+		try {
+			setIsLoading(true);
+			await fetch(`${BASE_URL}/cities/${id}`, {
+				method: "DELETE",
+			});
+			setCities((cities) => cities.filter((city) => city.id !== id));
+		} catch {
+			alert("Error deleting city.");
 		} finally {
 			setIsLoading(false);
 		}
 	}
 	return (
-		<CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity, createCity }}>
+		<CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity, createCity, deleteCity }}>
 			{children}
 		</CitiesContext.Provider>
 	);
--- 11-worldwise/starter/worldwise/src/pages/AppLayout.jsx
+++ 11-worldwise/starter/worldwise/src/pages/AppLayout.jsx
@@ -1,14 +1,14 @@
-import SideBar from "../components/SideBar"
-import Map from "../components/Map"
-import styles from './AppLayout.module.css'
+import SideBar from "../components/SideBar";
+import Map from "../components/Map";
+import styles from "./AppLayout.module.css";
 
 function AppLayout() {
-  return (
-    <div className={styles.app}>
-      <SideBar /> 
-      <Map />
-    </div>
-  )
+	return (
+		<div className={styles.app}>
+			<SideBar />
+			<Map />
+		</div>
+	);
 }
 
-export default AppLayout
+export default AppLayout;
