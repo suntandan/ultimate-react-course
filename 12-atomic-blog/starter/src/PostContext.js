5d6224a3f07d3abb4e7e4009e8a14a73f5e51e90
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-07-16 15:29:19 +0200

    Challenge 2: Recactoring React Quiz to Context API

--- 12-atomic-blog/starter/src/PostContext.js
+++ 12-atomic-blog/starter/src/PostContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
