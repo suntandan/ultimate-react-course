f96aad073a6a999f69e6c567fdcd59f947ffa8fc
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-07-27 16:38:32 +0200

    Building the Cart page

--- 16-fast-react-pizza/Project/fast-react-pizza/src/features/cart/cartSlice.js
+++ 16-fast-react-pizza/Project/fast-react-pizza/src/features/cart/cartSlice.js
@@ -42,6 +42,8 @@ export const {
 
 export default cartSlice.reducer;
 
+export const getUser = (state) => state.user.username;
+export const getCart = (state) => state.cart.cart;
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 export const getTotalCartPrice = (state) =>
