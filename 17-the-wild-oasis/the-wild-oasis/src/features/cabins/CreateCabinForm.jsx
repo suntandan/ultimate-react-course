211eb401d2a91227d99d3a3362b79b078dfa2064
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-07-29 18:03:30 +0200

    355: Uploading images to supabase

--- 17-the-wild-oasis/the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -25,7 +25,7 @@ function CreateCabinForm() {
 	});
 
 	function onSubmit(data) {
-		mutate(data);
+		mutate({ ...data, image: data.image[0] });
 	}
 
 	function onError(errors) {
@@ -91,7 +91,7 @@ function CreateCabinForm() {
 			</FormRow>
 
 			<FormRow label="Cabin photo">
-				<FileInput id="image" accept="image/*" />
+				<FileInput id="image" accept="image/*" {...register("image", { required: "This field is required." })} />
 			</FormRow>
 
 			<FormRow>
--- 17-the-wild-oasis/the-wild-oasis/src/services/supabase.js
+++ 17-the-wild-oasis/the-wild-oasis/src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://pdlndfzmjcgtcuqyswwh.supabase.co";
+export const supabaseUrl = "https://pdlndfzmjcgtcuqyswwh.supabase.co";
 const supabaseKey =
 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkbG5kZnptamNndGN1cXlzd3doIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIxOTEyMDEsImV4cCI6MjAzNzc2NzIwMX0.aLrEADxm5AicgKkH8Rgf4bpfL38Fw6U1mdQ2T1ah1EY";
 
