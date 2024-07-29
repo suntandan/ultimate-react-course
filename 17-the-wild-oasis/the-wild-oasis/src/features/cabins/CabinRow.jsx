152ca98b504b5ac204a8034356784c2e03f93b53
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-07-29 12:10:37 +0200

    351: Displaying toasts (Notifications)

--- 17-the-wild-oasis/the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
 	display: grid;
@@ -51,11 +52,11 @@ function CabinRow({ cabin }) {
 	const { isPending, mutate } = useMutation({
 		mutationFn: deleteCabin,
 		onSuccess: () => {
-			alert("Cabin deleted successfully");
 			queryClient.invalidateQueries({ queryKey: ["cabins"] });
+			toast.success("Cabin deleted successfully");
 		},
 
-		onError: (err) => alert(err.message),
+		onError: (err) => toast.error(err.message),
 	});
 
 	return (
