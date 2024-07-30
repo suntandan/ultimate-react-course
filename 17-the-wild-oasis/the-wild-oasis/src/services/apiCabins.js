0edfd4c32779997145097f7801fd8a84878cfe73
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-07-30 14:51:37 +0200

    358: Duplicating cabins

--- 17-the-wild-oasis/the-wild-oasis/src/services/apiCabins.js
+++ 17-the-wild-oasis/the-wild-oasis/src/services/apiCabins.js
@@ -33,6 +33,7 @@ export async function createEditCabin(newCabin, id) {
 	}
 
 	// 2. Upload image
+	if (hasImagePath) return data;
 	const { error: storageError } = await supabase.storage.from("cabin-images").upload(imageName, newCabin.image);
 
 	// 3. Delete the cabin IF there was an error uploading the image
