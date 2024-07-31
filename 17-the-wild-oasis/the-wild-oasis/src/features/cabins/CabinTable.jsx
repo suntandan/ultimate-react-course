bb7fe9d1de1ce6ef7a9a7c32ac0f9e79b0a916dd
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-07-31 18:59:39 +0200

    376: Client side sorting: Sorting Cabins

--- 17-the-wild-oasis/the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -11,6 +11,7 @@ function CabinTable() {
 
 	if (isPending) return <Spinner />;
 
+	// 1. FILTER
 	const filterValue = searchParams.get("discount") || "all";
 
 	let filteredCabins;
@@ -18,6 +19,15 @@ function CabinTable() {
 	if (filterValue === "all") filteredCabins = cabins;
 	if (filterValue === "no-discount") filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
 	if (filterValue === "with-discount") filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+	// 2. SORT
+	const sortBy = searchParams.get("sortBy") || "startDate-asc";
+	const [field, direction] = sortBy.split("-");
+	const modifier = direction === "asc" ? 1 : -1;
+	const sortedCabins = filteredCabins?.sort((a, b) =>
+		typeof a[field] === "string" ? a[field].localeCompare(b[field]) * modifier : (a[field] - b[field]) * modifier
+	);
+
 	return (
 		<Menus>
 			<Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -29,7 +39,7 @@ function CabinTable() {
 					<div>Discount</div>
 					<div></div>
 				</Table.Header>
-				<Table.Body data={filteredCabins} render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />} />
+				<Table.Body data={sortedCabins} render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />} />
 			</Table>
 		</Menus>
 	);
