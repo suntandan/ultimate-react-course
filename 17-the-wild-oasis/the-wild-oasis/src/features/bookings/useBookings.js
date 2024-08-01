1ab21bc343a20a8abb9f230e3b8d3fe389f05ece
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-08-01 11:08:18 +0200

    380: API-side Sorting: Sorting Bookings

--- 17-the-wild-oasis/the-wild-oasis/src/features/bookings/useBookings.js
+++ 17-the-wild-oasis/the-wild-oasis/src/features/bookings/useBookings.js
@@ -9,13 +9,18 @@ export function useBookings() {
 	const filterValue = searchParams.get("status");
 	const filter = !filterValue || filterValue === "all" ? null : { field: "status", value: filterValue };
 
+	// SORT
+	const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+	const [field, direction] = sortByRaw.split("-");
+	const sortBy = { field, direction };
+
 	const {
 		isPending,
 		data: bookings,
 		error,
 	} = useQuery({
-		queryKey: ["bookings", filter],
-		queryFn: () => getBookings({ filter }),
+		queryKey: ["bookings", filter, sortBy],
+		queryFn: () => getBookings({ filter, sortBy }),
 	});
 
 	return { isPending, bookings, error };
