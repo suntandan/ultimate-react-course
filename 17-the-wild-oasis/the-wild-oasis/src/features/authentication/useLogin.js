38ccbdd9644f216e44d888ddabacada0ef5e876e
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-08-06 11:16:44 +0200

    389: Authentication: User Login with Supabase

---
+++ 17-the-wild-oasis/the-wild-oasis/src/features/authentication/useLogin.js
@@ -0,0 +1,22 @@
+import { useMutation } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export function useLogin() {
+	const navigate = useNavigate();
+
+	const { mutate: login, isPending } = useMutation({
+		mutationFn: ({ email, password }) => loginApi({ email, password }),
+		onSuccess: () => {
+			navigate("/dashboard");
+		},
+
+		onError: (err) => {
+			console.log("ERROR", err);
+			toast.error("Provided email or password is incorrect");
+		},
+	});
+
+	return { login, isPending };
+}
