6e35b4bb40bab1bb63ca937658d2e69dd5ba2242
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-10-22 10:22:07 +0200

    Refactored and added suspense for cabin list

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
@@ -1,15 +1,10 @@
-import CabinCard from "@/app/_components/CabinCard";
-import { getCabins } from "@/app/_lib/data-service";
+import CabinList from "../_components/CabinList";
 
 export const metadata = {
 	title: "Cabins",
 };
 
 export default async function Page() {
-	console.log("Starting...");
-	const cabins = await getCabins();
-	console.log(cabins);
-
 	return (
 		<div>
 			<h1 className="text-4xl mb-5 text-accent-400 font-medium">Our Luxury Cabins</h1>
@@ -20,13 +15,7 @@ export default async function Page() {
 				peaceful, calm vacation. Welcome to paradise.
 			</p>
 
-			{cabins.length > 0 && (
-				<div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-					{cabins.map((cabin) => (
-						<CabinCard cabin={cabin} key={cabin.id} />
-					))}
-				</div>
-			)}
+			<CabinList />
 		</div>
 	);
 }
