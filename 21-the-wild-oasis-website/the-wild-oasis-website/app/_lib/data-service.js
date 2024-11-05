d88ffa49f46ff372e7942ccf569969dba6866471
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-11-05 18:35:35 +0100

    Not found errors

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/data-service.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/data-service.js
@@ -1,6 +1,7 @@
 import { eachDayOfInterval } from "date-fns";
 
 import { supabase } from "./supabase";
+import { notFound } from "next/navigation";
 
 /////////////
 // GET
@@ -13,6 +14,7 @@ export async function getCabin(id) {
 
 	if (error) {
 		console.error(error);
+		notFound();
 	}
 
 	return data;
