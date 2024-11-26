04b0e2a1a8f1e76a554ece6829751b88fc5b5a1e
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-11-26 10:52:38 +0100

    asdklj

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -1,7 +1,10 @@
+import Reservation from "@/app/_components/Reservation";
+import Spinner from "@/app/_components/Spinner";
 import TextExpander from "@/app/_components/TextExpander";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export async function generateMetadata({ params }) {
 	const cabin = await getCabin(params.cabinId);
@@ -12,8 +15,6 @@ export async function generateStaticParams() {
 	const cabins = await getCabins();
 
 	const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
-	console.log(ids);
-
 	return ids;
 }
 
@@ -61,7 +62,12 @@ export default async function Page({ params }) {
 			</div>
 
 			<div>
-				<h2 className="text-5xl font-semibold text-center">Reserve today. Pay on arrival.</h2>
+				<h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
+					Reserve {name} today. Pay on arrival.
+				</h2>
+				<Suspense fallback={<Spinner />}>
+					<Reservation cabin={cabin} />
+				</Suspense>
 			</div>
 		</div>
 	);
