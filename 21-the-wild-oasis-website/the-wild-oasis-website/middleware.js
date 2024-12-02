8d1b4cd3998e0174ccf93fc1bf8eecab74ccd62f
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-12-02 13:51:38 +0100

    Protecting Routes with NextAuth Middleware

--- 21-the-wild-oasis-website/the-wild-oasis-website/middleware.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/middleware.js
@@ -1,3 +1,14 @@
-export function middleware(request) {
-	console.log(request);
-}
+// import { NextResponse } from "next/server";
+
+// export function middleware(request) {
+// 	console.log(request);
+
+// 	return NextResponse.redirect(new URL("/about", request.url));
+// }
+
+import { auth } from "@/app/_lib/Auth";
+
+export const middleware = auth;
+export const config = {
+	matcher: ["/account"],
+};
