94867e64cbead3a9ca8fa701152f7e537ec09d43
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-12-02 09:42:46 +0100

    Setting up authentication

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/Navigation.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/Navigation.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { auth } from "@/app/_lib/Auth";
 
-export default function Navigation() {
+export default async function Navigation() {
+	const session = await auth();
+	console.log(session);
+
 	return (
 		<nav className="z-10 text-xl">
 			<ul className="flex gap-16 items-center">
@@ -15,9 +19,21 @@ export default function Navigation() {
 					</Link>
 				</li>
 				<li>
-					<Link href="/account" className="hover:text-accent-400 transition-colors">
-						Guest area
-					</Link>
+					{session?.user?.image ? (
+						<Link href="/account" className="hover:text-accent-400 transition-colors flex items-center gap-4">
+							<img
+								src={session.user.image}
+								className="h-8 rounded-full"
+								alt="User avatar"
+								referrerPolicy="no-referrer"
+							/>
+							<span>Guest area</span>
+						</Link>
+					) : (
+						<Link href="/account" className="hover:text-accent-400 transition-colors">
+							Guest area
+						</Link>
+					)}
 				</li>
 			</ul>
 		</nav>
