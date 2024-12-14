adef6406ee413d6e92ed1ddaf41e54081a3dbb3a
Dan Lorntzsen <dan.lorntzsen@bas.no>
2024-12-14 11:47:37 +0100

    Using the useOptimistic hook

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/ReservationCard.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/ReservationCard.js
@@ -1,7 +1,7 @@
 import { PencilSquareIcon } from "@heroicons/react/24/solid";
 import { format, formatDistance, isPast, isToday, parseISO } from "date-fns";
-import Link from "next/link";
 import DeleteReservation from "@/app/_components/DeleteReservation";
+import Link from "next/link";
 import Image from "next/image";
 
 export const formatDistanceFromNow = (dateStr) =>
@@ -9,7 +9,7 @@ export const formatDistanceFromNow = (dateStr) =>
 		addSuffix: true,
 	}).replace("about ", "");
 
-function ReservationCard({ booking }) {
+function ReservationCard({ booking, onDelete }) {
 	const {
 		id,
 		guestId,
@@ -72,7 +72,7 @@ function ReservationCard({ booking }) {
 							<PencilSquareIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
 							<span className="mt-1">Edit</span>
 						</Link>
-						<DeleteReservation bookingId={id} />
+						<DeleteReservation bookingId={id} onDelete={onDelete} />
 					</>
 				) : null}
 			</div>
---
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/ReservationList.js
@@ -0,0 +1,24 @@
+"use client";
+
+import ReservationCard from "./ReservationCard";
+import { deleteReservation } from "../_lib/actions";
+import { useOptimistic } from "react";
+
+function ReservationList({ bookings }) {
+	const [optimisticBookings, optimisticDelete] = useOptimistic(bookings, (curBookings, bookingId) => {
+		return curBookings.filter((booking) => booking.id !== bookingId);
+	});
+	async function handleDelete(bookingId) {
+		optimisticDelete(bookingId);
+		await deleteReservation(bookingId);
+	}
+	return (
+		<ul className="space-y-6">
+			{optimisticBookings.map((booking) => (
+				<ReservationCard booking={booking} key={booking.id} onDelete={handleDelete} />
+			))}
+		</ul>
+	);
+}
+
+export default ReservationList;
